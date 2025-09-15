d2f0f03e723b396aab678b51ee39524311384f67
Istvan Zilizi <istvan.zilizi@outlook.com>
2025-09-15 22:31:51 +0200

    playing with <iomanip>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <initializer_list>
 #include <cassert>
+#include <array>
+#include <algorithm>
 #include "FixedSizedArray.hpp"
 
 
@@ -9,6 +12,14 @@ struct Entity{
 	std::string name;
 	int x;
 	int y;
+	Entity():name("Unknown"),x(0),y(0){}
+	Entity(std::string name, int xPos, int yPos):name(name),x(xPos),y(yPos){
+		std::cout << "Log: Created entity " << name << std::endl;
+	}
+	~Entity(){
+		std::cout << "Log: Destroyed entity " << name << std::endl;
+	}
+
 };
 
 
@@ -17,25 +28,27 @@ struct Entity{
 int main(){
 
 	Entity e{};
+	std::array<Entity,2> arr = {Entity("Player",1,2), Entity("Enemy",3,4)};
+	
 	using T = FixedSizedArray<int>;
-	std::cout << "Test FixedSizedArray" << std::endl;
+	std::cout << std::left << std::setw(30) << std::setfill('.') << "Test FixedSizedArray" << std::endl;
 	T c(5);
 	T c2 {5};
-	std::cout << "Entity test" << std::endl;
+	std::cout << std::setw(30) << std::setfill('*') << "Entity test" << std::endl;
 	std::cout << e.name << std::endl;
 	std::cout << e.x << std::endl;
 	std::cout << e.y << std::endl;
 
 
-	std::cout << "Test of the FixedSizedArray" << std::endl;
+	std::cout << std::setw(30) << std::setfill('.') << "Test of the FixedSizedArray" << std::endl;
 	c[4]=-15;
 
-	std::cout<< "c : " << std::endl;
+	std::cout << std::setw(30) << std::setfill('.') << "c : " << std::endl;
 	for(const auto& e : c){
 		std::cout<< e << std::endl;
 	}
 	
-	std::cout<< "c2 : " << std::endl;
+	std::cout << std::setw(30) << std::setfill('.') << "c2 : " << std::endl;
 	for(const auto& e : c2){
 		std::cout<< e << std::endl;
 	}
